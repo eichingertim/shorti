e565d89b9d48c6001eb1288b91058607c4cb0153
Tim Eichinger <privat.timeichinger@gmail.com>
2022-02-25 13:21:36 +0100

    Setup frontend and backend module

---
+++ shorti-service/src/main/java/me/timeichinger/shortiservice/ShortiServiceApplication.java
@@ -0,0 +1,13 @@
+package me.timeichinger.shortiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortiServiceApplication.class, args);
+	}
+
+}
---
+++ shorti-service/src/test/java/me/timeichinger/shortiservice/ShortiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.timeichinger.shortiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
